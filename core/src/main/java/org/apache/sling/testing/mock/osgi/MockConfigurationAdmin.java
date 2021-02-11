f4ea9e8a37c3fdbe3d4a61f4d78a50c73c64cfad
Henry Kuijpers <Henry.Kuijpers@amplexor.com>
2021-02-11 19:48:41 +0100

    SLING-10138 Add proper throws declarations for the MockConfigurationAdmin and provide implementation for listConfigurations

--- core/src/main/java/org/apache/sling/testing/mock/osgi/MockConfigurationAdmin.java
+++ core/src/main/java/org/apache/sling/testing/mock/osgi/MockConfigurationAdmin.java
@@ -18,22 +18,30 @@
  */
 package org.apache.sling.testing.mock.osgi;
 
+import org.apache.felix.framework.FilterImpl;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Filter;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
 import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-
-import org.osgi.service.cm.Configuration;
-import org.osgi.service.cm.ConfigurationAdmin;
 
 /**
  * Mock implementation of {@link ConfigurationAdmin}.
  */
 class MockConfigurationAdmin implements ConfigurationAdmin {
-    
-    private ConcurrentMap<String, Configuration> configs = new ConcurrentHashMap<String, Configuration>();
+    private final BundleContext bundleContext;
+    private final ConcurrentMap<String, Configuration> configs = new ConcurrentHashMap<>();
+
+    MockConfigurationAdmin(final BundleContext bundleContext) {
+        this.bundleContext = bundleContext;
+    }
 
     @Override
-    public Configuration getConfiguration(String pid) {
+    public Configuration getConfiguration(final String pid) throws IOException {
         configs.putIfAbsent(pid, new MockConfiguration(pid));
         return configs.get(pid);
     }
@@ -41,32 +49,42 @@ class MockConfigurationAdmin implements ConfigurationAdmin {
     // --- unsupported operations ---
 
     @Override
-    public Configuration getConfiguration(String pid, String location) {
+    public Configuration getConfiguration(final String pid, final String location) throws IOException {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Configuration createFactoryConfiguration(String factoryPid) {
+    public Configuration createFactoryConfiguration(final String factoryPid) throws IOException {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Configuration createFactoryConfiguration(String factoryPid, String location) {
+    public Configuration createFactoryConfiguration(final String factoryPid, final String location) throws IOException {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Configuration[] listConfigurations(String filter) {
+    @SuppressWarnings("squid:S1168")
+    public Configuration[] listConfigurations(final String filter) throws IOException, InvalidSyntaxException {
+        final Filter filterObject = bundleContext.createFilter(filter);
+        final Configuration[] filtered = configs
+                .values()
+                .stream()
+                .filter(configuration -> filterObject.match(configuration.getProperties()))
+                .toArray(Configuration[]::new);
+        if (filtered.length != 0) {
+            return filtered;
+        }
+        return null;
+    }
+
+    @Override
+    public Configuration getFactoryConfiguration(final String factoryPid, final String name, final String location) throws IOException {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public Configuration getFactoryConfiguration(String factoryPid, String name, String location) throws IOException {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public Configuration getFactoryConfiguration(String factoryPid, String name) throws IOException {
+    public Configuration getFactoryConfiguration(final String factoryPid, final String name) throws IOException {
         throw new UnsupportedOperationException();
     }
 
